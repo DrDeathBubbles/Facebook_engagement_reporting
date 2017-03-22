adfe5ef3823e1fb944596fa56e095e2318e00093
DrDeathBubbles <aaron.meagher@gmail.com>
2017-03-22 14:34:52 +0000

    Adding json loads of dictionary

--- Initianl_reporting.py
+++ Initianl_reporting.py
@@ -1,12 +1,12 @@
 import requests
-from requests_oauthlib import OAuth1
 import os
-
+import json
 #r = requests.get('https://graph.facebook.com/v2.8/WebSummitHQ/feed',
 #        headers = {'access_token':os.environ['ACCESSTOKEN']})
-#
+
+
 
 r = requests.get('https://graph.facebook.com/v2.8/WebSummitHQ/feed?access_token={}'.format(os.environ['ACCESSTOKEN']))
+data = json.loads(r.text)
 
-
-print(r.text)
+print(data)
